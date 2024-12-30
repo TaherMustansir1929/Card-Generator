e6aa868a4d9f7ca725d35b28d0f0617a66b5eaf4
TaherMustansir1929 <eren.jeager1929101@gmail.com>
2024-12-30 16:47:25 +0500

    Second try - Successful

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,28 @@
 import { useEffect, useState } from 'react'
 import Card from './components/Card'
+import generateImage from './utils/generateImage';
 
 function App() {
 
-  const apiData = async () => {
-    const response = await fetch('https://jsonplaceholder.typicode.com/posts')
-    const data = await response.json()
-    return data
-  }
-
   const [data, setData] = useState([])
 
-  useEffect(() => {
-    apiData().then(data => setData(data))
+  const fetchData = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+    const apiData = await res.json()
+    setData(apiData)
+  }
 
-    return (
-      <div className="flex justify-center items-center min-h-screen bg-gray-100 px-4">
-        <div className="flex justify-between w-[1600px]">
-          {data.map((card)=>{
-            return (
-              <Card key={card.id} title={card.title} description={card.body} />
-            )
-          })}
-        </div>
-      </div>
-    )
+  useEffect(() => {
+    fetchData();
   }, [])
+
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100 px-4 pt-5">
+      <div className="flex gap-5 justify-evenly flex-wrap">
+        {data.map((card) => <Card key={card.id} title={card.title} description={card.body} image={generateImage()} />)}
+      </div>
+    </div>
+  )
 }
 
 export default App
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import generateImage from '../utils/generateImage';
 
-const Card = ({ title, description }) => {
+const Card = ({ title, description, image }) => {
   return (
     <div className="w-1/3 max-w-[500px] min-w-[300px] mx-2 rounded overflow-hidden shadow-lg bg-white">
-      <img className="w-full h-40 object-cover" src={generateImage} alt={title} />
+      <img className="w-full h-40 object-cover" src={image} alt={title} />
       <div className="px-4 py-2">
         <div className="font-bold text-lg mb-1">{title}</div>
         <p className="text-gray-700 text-sm">{description}</p>
